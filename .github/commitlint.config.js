d2dd445d3a2e734c5e62c4c56049af89e33c8475
Soham-ambibuzz <soham.pawar@ambibuzz.com>
2025-12-17 13:53:11 +0530

    Feat/ab 05632 (#2)
    * feat: Initial addition of ERPNext Balance Sheet base for BIR
    
    * feat: Added github actions
    
    * feat: Added account level selection filter to balance sheet
    
    - Renamed Balance Sheet Report to Balance Sheet BIR Report
    - Implements a custom filter in the PFRS Balance Sheet report allowing users to
    specify the maximum level of account hierarchy to display (e.g., Level 1-4 or All),
    - Improving report clarity for high-level review.
    
    Signed-off-by: Soham-ambibuzz <soham.pawar@ambibuzz.com>
    AB-05632
    
    * feat: Balance Sheet BIR: (#3)
    
    - Rename "Total Asset (Debit)" to "Total Current Assets (Debit)"
    - Rename "Total Liability (Credit)" to "Total Current Liabilities (Credit)"
    - Hide top-level amounts using report formatter
    
    
    AB-05664
    
    Signed-off-by: Soham-ambibuzz <soham.pawar@ambibuzz.com>
    
    * feat: updated the README file
    
    Signed-off-by: Soham-ambibuzz <soham.pawar@ambibuzz.com>
    AB-05632
    
    ---------
    
    Signed-off-by: Soham-ambibuzz <soham.pawar@ambibuzz.com>

---
+++ .github/commitlint.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+
+	rules: {
+		"type-enum": [
+			2,
+			"always",
+			["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"],
+		],
+		"subject-empty": [2, "never"],
+		"subject-case": [0],
+		"type-case": [2, "always", "lower-case"],
+		"type-empty": [2, "never"],
+		"signed-off-by": [2, "always", "Signed-off-by"],
+	},
+};

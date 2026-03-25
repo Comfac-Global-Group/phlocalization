cc1985b1e8725e3946bfe3a0b7677da54cb830ba
phlocalization <250944488+phlocalization@users.noreply.github.com>
2026-03-25 15:12:55 +0800

    Add files via upload
    Fixed the Project filter in the Project in Progress Subsidiary report. Users can now select a specific Project ID and the report will correctly display only the matching project. Wildcard searches (e.g., `%PROJ-2%`) are also working as expected.

---
+++ bureau_of_internal_revenue-development/bureau_of_internal_revenue/bureau_of_internal_revenue/report/accounts_payable_transaction_listing/accounts_payable_transaction_listing.js
@@ -0,0 +1,33 @@
+// Copyright (c) 2026, Ambibuzz Technologies LLP and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Accounts Payable Transaction Listing"] = {
+	"filters": [
+		{
+			fieldname: "company",
+			label: "Company",
+			fieldtype: "Link",
+			options: "Company",
+			reqd: 1,
+			default: frappe.defaults.get_default("company")
+		},
+		{
+			fieldname: "from_date",
+			label: "From Date",
+			fieldtype: "Date",
+			reqd: 1
+		},
+		{
+			fieldname: "to_date",
+			label: "To Date",
+			fieldtype: "Date",
+			reqd: 1
+		},
+		{
+			fieldname: "doc_type",
+			label: "Doc Type",
+			fieldtype: "Select",
+			options: "\nPurchase Invoice\nJournal Entry"
+		}
+	]
+};

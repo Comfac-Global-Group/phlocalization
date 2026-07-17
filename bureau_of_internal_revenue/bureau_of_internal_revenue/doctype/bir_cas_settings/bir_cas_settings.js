75c15d9f79033179d3bb8faef62997d88f055ba7
Soham Pawar <soham.pawar@ambibuzz.com>
2026-07-17 13:23:38 +0530

    feature: add BIR CAS Settings singleton doctype
    Signed-off-by: Soham Pawar <soham.pawar@ambibuzz.com>

---
+++ bureau_of_internal_revenue/bureau_of_internal_revenue/doctype/bir_cas_settings/bir_cas_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ambibuzz Technologies LLP and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("BIR CAS Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
